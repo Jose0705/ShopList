bcc94064c465518c15b68061c94fc87a60f4c185
José Flores <24470240@campeche.tecnm.mx>
2025-11-21 10:40:14 -0600

    Utilaza el patron MVVM sin biblotecas

--- ShopList.Gui/ViewModels/ShopListViewModels.cs
+++ ShopList.Gui/ViewModels/ShopListViewModels.cs
@@ -1,47 +1,107 @@
 ﻿using ShopList.Gui.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+
 
 namespace ShopList.Gui.ViewModels
-{
-
-namespace ShopList.Gui.ViewModels
-{
-    public class ShopListViewModels
     {
-        public class NewPague1{
-        public ObservableCollection<Item> item { get;}
-                public NewPague1()
+    public class ShopListViewModels : INotifyPropertyChanged
+    {
+
+        private string _nombreDelArticulo = string.Empty;
+        private int _cantidadAComprar = 1;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ObservableCollection<Item> item { get; }
+        public string NombreDelArticulo
+        {
+            get => _nombreDelArticulo;
+            set
             {
-                item = new ObservableCollection<Item>();
-                CargarDatos();
-            }
-            public void CargarDatos()
-            {
-                item.Add(new Item
+                if (value != _nombreDelArticulo)
                 {
-                    Id = 1,
-                    Nombre = "",
-                    Cantidad = 11
-                });
-                item.Add(new Item
-                {
-                    Id = 2,
-                    Nombre = "",
-                    Cantidad = 12
-                });
-                item.Add(new Item
-                {
-                    Id = 3,
-                    Nombre = "",
-                    Cantidad = 13
-                });
+                    _nombreDelArticulo = value;
+                    OnPropetyChanged(nameof(NombreDelArticulo));
+                }
             }
         }
-    }
-}}
+        public int CantidadAComprar
+        {
+            get => _cantidadAComprar;
+            set
+            {
+                if (value != _cantidadAComprar)
+                {
+                    _cantidadAComprar = value;
+                    OnPropetyChanged(nameof(CantidadAComprar));
+                }
+            }
+        }
+        public ICommand AgregarShopListItemCommand { get; private set; }
+        
+        public ShopListViewModels()
+        {
+            item = new ObservableCollection<Item>();
+            CargarDatos();
+            AgregarShopListItemCommand = new Command(AgregarShopListItem);
+        }
+        public void AgregarShopListItem()
+        {
+            if (string.IsNullOrEmpty(NombreDelArticulo) || CantidadAComprar <= 0)
+            {
+                return;
+            }
+            Random generador = new Random();
+
+            var Item = new Item
+            {
+                Id = generador.Next(),
+                Nombre = NombreDelArticulo,
+                Cantidad = CantidadAComprar,
+                Comprado = false,
+            };
+            item.Add(Item);
+            NombreDelArticulo = String.Empty;
+            CantidadAComprar = 1;
+        }
+
+        public void EleminarShopListItem()
+        {
+
+        }
+        public void CargarDatos()
+        {
+            item.Add(new Item
+            {
+                Id = 1,
+                Nombre = "leche",
+                Cantidad = 10,
+                Comprado = false
+            });
+            item.Add(new Item
+            {
+                Id = 2,
+                Nombre = "Huevo",
+                Cantidad = 22,
+                Comprado = false
+            });
+            item.Add(new Item
+            {
+                Id = 3,
+                Nombre = "Carne",
+                Cantidad = 32,
+                Comprado = true
+            });
+        }
+        private void OnPropetyChanged(string propetyName)
+        {
+            PropertyChanged?.Invoke(
+                this, new PropertyChangedEventArgs(propetyName)
+                );
+        }
+        }
+    }    
+
